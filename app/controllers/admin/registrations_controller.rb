510b1e05eba2dbe8be861c34acf13a72018ad52f
EC2 Default User <ec2-user@ip-172-31-15-72.ap-northeast-1.compute.internal>
2023-02-08 07:29:49 +0000

    サインアップ・サインイン機能更新

--- app/controllers/admin/registrations_controller.rb
+++ app/controllers/admin/registrations_controller.rb
@@ -59,4 +59,15 @@ class Admin::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  def after_sign_up_path_for(resource)
+    root_path
+  end
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :encrypted_password, :password_confirmation])
+  end
+  
 end
