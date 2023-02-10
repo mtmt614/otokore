472952f0828fec915512c91f9e7891de6c2d3d5e
EC2 Default User <ec2-user@ip-172-31-15-72.ap-northeast-1.compute.internal>
2023-02-10 15:24:56 +0000

    退会ステータスの追加

--- db/migrate/20230206074916_devise_create_users.rb
+++ db/migrate/20230206074916_devise_create_users.rb
@@ -6,6 +6,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.boolean :is_deleted, default: false
       
       t.string :name, null: false
 
--- db/schema.rb
+++ db/schema.rb
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 2023_02_09_070341) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.boolean "is_deleted", default: false
     t.string "name", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
