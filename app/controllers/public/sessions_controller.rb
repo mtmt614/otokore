70bbfa8bdc56ee04b3f7e2b3bbe990fed0f43f7c
EC2 Default User <ec2-user@ip-172-31-15-72.ap-northeast-1.compute.internal>
2023-02-10 15:27:46 +0000

    退会ステータスの追加

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -31,4 +31,13 @@ class Public::SessionsController < Devise::SessionsController
   def after_sign_out_path_for(resource)
     root_path
   end
+  
+  def user_state
+    @user = User.find_by(email: params[:user][:email])
+    return if !@user
+    if (@user.valid_password?(params[:user][:encrypted_password]) && (@user.is_deleted == true))
+      redirect_to new_user_registration_path
+    end
+  end
+  
 end
