b1cff75085e29bcfbd36015cd8589f90448b2ee7
EC2 Default User <ec2-user@ip-172-31-15-72.ap-northeast-1.compute.internal>
2023-02-10 15:37:27 +0000

    管理者側の会員閲覧・編集ページ作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,19 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
+  
+  has_one_attached :profile_image
+  
+  def get_profile_image(width, height)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'no_image.jpg', content_type: 'image/jpeg')
+    end
+      profile_image.variant(resize_to_limit: [width, height]).processed
+  end
+  
+  def active_for_authentication?
+    super && (self.is_deleted == false)
+  end
+  
 end
