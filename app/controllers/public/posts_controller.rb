58c17411a453183072d1711f6eaad3e3fa10e495
EC2 Default User <ec2-user@ip-172-31-15-72.ap-northeast-1.compute.internal>
2023-03-06 16:48:55 +0000

    画像投稿機能の作成

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -54,7 +54,7 @@ class Public::PostsController < ApplicationController
   private
   
    def post_params
-     params.require(:post).permit(:title, :artist, :content, :genre_id)
+     params.require(:post).permit(:title, :artist, :content, :genre_id, :image)
    end
    
    def ensure_guest_user
--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,16 @@ class Post < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
   
+  has_one_attached :image
+  
+  def get_image
+    if image.attached?
+      image
+    else
+      'onpu.jpg'
+    end
+  end
+  
   def liked_by?(user)
     likes.exists?(user_id: user.id)
   end
