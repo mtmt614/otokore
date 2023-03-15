ac6810483546e8c257f2ea2fc02d14a90d6a5044
EC2 Default User <ec2-user@ip-172-31-15-72.ap-northeast-1.compute.internal>
2023-03-15 05:39:52 +0000

    フラッシュメッセージ追加

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -2,6 +2,7 @@ class Admin::CommentsController < ApplicationController
   
   def destroy
     Comment.find(params[:id]).destroy
+    flash[:notice] = 'コメントを削除しました'
     redirect_to admin_post_path(params[:post_id])
   end
   
