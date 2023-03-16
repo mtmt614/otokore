1b19b7f41775d9faa911d6a120d581b961e0557b
EC2 Default User <ec2-user@ip-172-31-15-72.ap-northeast-1.compute.internal>
2023-03-16 10:30:43 +0000

    [Fix] Comment

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,6 +1,6 @@
 class Public::CommentsController < ApplicationController
   before_action :ensure_guest_user, only: [:create, :destroy]
-  
+
   def create
     post = Post.find(params[:post_id])
     comment = current_user.comments.new(comment_params)
@@ -8,23 +8,24 @@ class Public::CommentsController < ApplicationController
     comment.save
     redirect_to post_path(params[:post_id])
   end
-  
+
   def destroy
+    post = Post.find(params[:post_id])
     @comments = post.comments.page(params[:page]).per(3)
     current_user.comments.find(params[:id]).destroy!
     flash[:notice] = 'コメントを削除しました'
     redirect_to post_path(params[:post_id])
   end
-  
+
   private
   def comment_params
     params.require(:comment).permit(:body)
   end
-  
+
   def ensure_guest_user
     if current_user.email == "guest@email.com"
       redirect_to user_path(current_user) , notice: 'ゲストユーザーはこの操作を行うことができません。'
     end
-  end  
-  
+  end
+
 end
